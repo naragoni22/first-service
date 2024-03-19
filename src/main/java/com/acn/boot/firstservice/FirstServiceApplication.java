736b2da852ca2ca49d7084b19bf63d088e4461dc
naragoni22 <naresh.naragoni20@gmail.com>
2024-03-19 12:15:32 +0530

    removed whitespaceds

--- src/main/java/com/acn/boot/firstservice/FirstServiceApplication.java
+++ src/main/java/com/acn/boot/firstservice/FirstServiceApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FirstServiceApplication {
-
-	public static void main(String[] args) {
+        public static void main(String[] args) {
 		SpringApplication.run(FirstServiceApplication.class, args);
 		System.out.println("hello world");
 		
