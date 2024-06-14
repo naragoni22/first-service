86dcb8770449591e112c9db04785b7301fcac32b
naragoni22 <naresh.naragoni20@gmail.com>
2024-06-14 20:40:23 +0530

    adding chnages

--- src/main/java/com/acn/boot/firstservice/FirstServiceApplication.java
+++ src/main/java/com/acn/boot/firstservice/FirstServiceApplication.java
@@ -8,6 +8,7 @@ public class FirstServiceApplication {
         public static void main(String[] args) {
 		SpringApplication.run(FirstServiceApplication.class, args);
 		System.out.println("hello world");
+		System.out.println("hi java");
 		
 	}
 
